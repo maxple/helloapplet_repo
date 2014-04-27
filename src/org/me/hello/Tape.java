f097f421b17bea2df04a128c9a32e31ccaebcd7a
Max Ple <maxple1@gmail.com>
2014-04-27 11:39:41 +0400

    tape nums increment ok

--- src/org/me/hello/Tape.java
+++ src/org/me/hello/Tape.java
@@ -2,24 +2,51 @@ package org.me.hello;
 
 import java.applet.Applet;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Tape extends Applet implements Runnable {
 
     Thread th;
+    
+    Integer x;
 
     @Override
     public void init() {
-        setBackground(new Color(255, 255, 255));
-        th = new Thread();
+        setBackground(Color.white);
+        setForeground(Color.black);
+        th = new Thread(this);
+        x = 0;
     }
 
     @Override
     public void start() {
         th.start();
+        /*while(true) {
+            x++;
+        }*/
     }
 
     @Override
     public void run() {
-        getGraphics().drawString("hello", 50, 50);
+
+        Graphics g = getGraphics();
+
+        while (true) {
+            g.setColor(Color.black);
+            g.drawString(x.toString(), 50, 50);
+            
+            /*try {
+                Thread.sleep(1);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Tape.class.getName()).log(Level.SEVERE, null, ex);
+            }*/
+            
+            g.setColor(Color.white);
+            g.drawString(x.toString(), 50, 50);
+            
+            x++;
+        }
     }
 }
