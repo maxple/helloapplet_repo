0d01001ecc69a66a15d3d1fe1e48ae6097e8349a
Max Ple <maxple1@gmail.com>
2014-04-27 14:32:31 +0400

    keys set ok

--- src/org/me/hello/Tape.java
+++ src/org/me/hello/Tape.java
@@ -5,38 +5,63 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Tape extends Applet implements Runnable {
 
-    Thread th;
+    Thread th;   
 
-    Integer x;
+    Integer x, y;
 
     Image memImage;
     Graphics memImageGraphics;
     Dimension memImageDim;
 
     Graphics g;
-    
+
     int updates, paints, repaints;
+
+    Set<Integer> keysPressed = new HashSet<>();
 
     @Override
     public void init() {
-        
+
         updates = 0;
         paints = 0;
         repaints = 0;
-        
+
+        setFocusable(true);
+
+        addKeyListener(new KeyAdapter() {
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                
+                keysPressed.add(e.getKeyCode());
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                
+                keysPressed.remove(e.getKeyCode());
+            }
+        });
+
         setBackground(Color.white);
         setForeground(Color.black);
         g = getGraphics();
         th = new Thread(this);
         x = 0;
+        y = 0;
     }
 
     @Override
+
     public void start() {
         th.start();
     }
@@ -45,34 +70,65 @@ public class Tape extends Applet implements Runnable {
     public void run() {
 
         while (true) {
-            g.setColor(Color.black);
-            g.drawString(x.toString(), 50, 50);
-
             try {
-                Thread.sleep(100);
+                keyEventHandler();
+                repaint();
+                repaints++;
+                Thread.sleep(100);                
             } catch (InterruptedException ex) {
                 Logger.getLogger(Tape.class.getName()).log(Level.SEVERE, null, ex);
             }
-
-            g.setColor(Color.white);
-            g.drawString(x.toString(), 50, 50);
-
-            repaint();
-
-            repaints++;
-            
-            x++;
         }
     }
 
     @Override
     public void update(Graphics g) {
+
         updates++;
+
+        int w, h;
+
+        w = getBounds().width;
+        h = getBounds().height;
+
+        if ((memImageDim == null) || (memImageDim.width != w) || (memImageDim.height != h)) {
+            memImageDim = new Dimension(w, h);
+            memImage = createImage(w, h);
+            memImageGraphics = memImage.getGraphics();
+        }
+
+        Color cbg = getBackground();
+        Color cfg = getForeground();
+
+        memImageGraphics.setColor(cbg);
+
+        memImageGraphics.fillRect(0, 0, w, h);
+
+        memImageGraphics.setColor(cfg);
+
+        memImageGraphics.drawString(x.toString(), 50, 50);
+
+        memImageGraphics.drawString(y.toString(), 50, 100);
+        
+        memImageGraphics.drawString(keysPressed.toString(), 50, 150);
+
         paint(g);
     }
 
     @Override
     public void paint(Graphics g) {
+
         paints++;
+
+        if (memImage != null) {
+            g.drawImage(memImage, 0, 0, null);
+        }
+    }
+    
+    void keyEventHandler() {
+        if (keysPressed.contains(KeyEvent.VK_UP)) y++;
+        if (keysPressed.contains(KeyEvent.VK_DOWN)) y--;
+        if (keysPressed.contains(KeyEvent.VK_LEFT)) x--;
+        if (keysPressed.contains(KeyEvent.VK_RIGHT)) x++;
     }
 }
